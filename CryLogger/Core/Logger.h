84df00ad1bd350b6250a2e8506f502cbe1fc6e95
Jacster0 <jacob.ogren2@gmail.com>
2021-07-23 00:49:19 +0200

    Removed unnecessary functions

--- CryLogger/Core/Logger.h
+++ CryLogger/Core/Logger.h
@@ -27,20 +27,14 @@ private:
 	Logger& operator=(Logger&& rhs)      = delete;
 	~Logger()                            = default;
 
-	constexpr void InternalLog(LogLevel lvl, const std::source_location& loc, auto&&... args) const noexcept;
-	constexpr void InternalFormatLog(LogLevel lvl, const std::source_location& loc, std::string_view fmt, auto&&... args) const noexcept;
-
 	std::mutex m_loggingMutex;
 	static std::mutex m_sinkMutex;
 	std::unordered_map<std::string_view, std::shared_ptr<ISinkBase>> m_sinks;
 };
 
-constexpr void Logger::Log(LogLevel lvl, const std::source_location& loc, auto && ...args) noexcept {
+constexpr void Logger::Log(LogLevel lvl, const std::source_location& loc, auto&& ...args) noexcept {
 	std::scoped_lock lock(m_loggingMutex);
-	InternalLog(lvl, loc, std::forward<decltype(args)>(args)...);
-}
 
-constexpr void Logger::InternalLog(LogLevel lvl, const std::source_location& loc, auto&&... args) const noexcept {
 	const std::string& message = (std::stringstream{} << ... << args).str();
 
 	for (const auto& [key, sink] : m_sinks) {
@@ -50,21 +44,18 @@ constexpr void Logger::InternalLog(LogLevel lvl, const std::source_location& loc
 
 constexpr void Logger::FormatLog(LogLevel lvl, const std::source_location& loc, std::string_view fmt, auto&&... args) noexcept {
 	std::scoped_lock lock(m_loggingMutex);
-	InternalFormatLog(lvl, loc, fmt, std::forward<decltype(args)>(args)...);
-}
 
-constexpr void Logger::InternalFormatLog(LogLevel lvl, const std::source_location& loc, std::string_view fmt, auto&&... args) const noexcept {
-	std::string message = std::format(fmt, std::forward<decltype(args)>(args)...);
+	const std::string message = std::format(fmt, std::forward<decltype(args)>(args)...);
 
 	for (const auto& [key, sink] : m_sinks) {
 		sink->Emit(message, lvl, loc);
 	}
 }
 
-#define crylog_info(...)    Logger::Get().Log(LogLevel::info, std::source_location::current(), __VA_ARGS__)
+#define crylog_info(...)    Logger::Get().Log(LogLevel::info,    std::source_location::current(), __VA_ARGS__)
 #define crylog_warning(...) Logger::Get().Log(LogLevel::warning, std::source_location::current(), __VA_ARGS__)
-#define crylog_error(...)   Logger::Get().Log(LogLevel::error, std::source_location::current(), __VA_ARGS__)
+#define crylog_error(...)   Logger::Get().Log(LogLevel::error,   std::source_location::current(), __VA_ARGS__)
 
-#define cryfmtlog_info(fmt, ...)    Logger::Get().FormatLog(LogLevel::info, std::source_location::current(), fmt, __VA_ARGS__)
+#define cryfmtlog_info(fmt, ...)    Logger::Get().FormatLog(LogLevel::info,    std::source_location::current(), fmt, __VA_ARGS__)
 #define cryfmtlog_warning(fmt, ...) Logger::Get().FormatLog(LogLevel::warning, std::source_location::current(), fmt, __VA_ARGS__)
-#define cryfmtlog_error(fmt, ...)   Logger::Get().FormatLog(LogLevel::error, std::source_location::current(), fmt, __VA_ARGS__)
+#define cryfmtlog_error(fmt, ...)   Logger::Get().FormatLog(LogLevel::error,   std::source_location::current(), fmt, __VA_ARGS__)
